6c9550a5aad4abc7536e8a897cb4b4d7ef9680b0
Zdenekdi <dias.zd@gmail.com>
2026-05-19 10:06:18 +0200

    chore: remove temporary utility scripts for file extraction and testing

--- temp_read.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-try {
-    const stats = fs.statSync('/Users/zdenekdias/Downloads/LexisLocal_Sitova_Politika.docx');
-    console.log("SUCCESS! File size:", stats.size);
-} catch (e) {
-    console.error("ERROR reading file:", e.message);
-}
